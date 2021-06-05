aa6391086ffd4e5b04cc414da59068eeded5175c
Andrew <wing_andrew@hotmail.com>
2021-06-05 15:30:11 -0400

    Adding inquirer prompts for the 3 classes of employees.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Dependencies
 const inquirer = require("inquirer");
 const jest = require("jest");
 const fs = require("fs");
@@ -6,3 +7,61 @@ const fs = require("fs");
 const Intern = require("./library/intern");
 const Engineer = require("./library/engineer");
 const Manager = require("./library/manager");
+
+function teamBuilder () {
+
+    function addManager();
+    function addEngineer();
+    function addIntern();
+};
+
+function addManager(){
+    inquirer.prompt([
+        {
+            message: "What is the team manager's name?",
+            name: "name"
+        },
+        {
+            message: "What is the team manager's email address?",
+            name: "email"
+        },
+        {
+            message: "What is the team manager's office number?",
+            name: "office"
+        },
+    ]);
+};
+
+function addEngineer(){
+    inquirer.prompt([
+        {
+            message: "What is the engineer's name?",
+            name: "name"
+        },
+        {
+            message: "What is the engineer's email address?",
+            name: "email"
+        },
+        {
+            message: "What is the engineers GitHub username?",
+            name: "github"
+        },
+    ]);
+};
+
+function addIntern(){
+    inquirer.prompt([
+        {
+            message: "What is the inern's name?",
+            name: "name"
+        },
+        {
+            message: "What is the intern's email address?",
+            name: "email"
+        },
+        {
+            message: "What is the intern's school of record?",
+            name: "school"
+        },
+    ]);
+};

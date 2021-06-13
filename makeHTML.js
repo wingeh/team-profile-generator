b3ca04a42465a7d56252fd7194f633d12af31327
Andrew <wing_andrew@hotmail.com>
2021-06-12 20:02:02 -0400

    Building makeHTML function

---
+++ makeHTML.js
@@ -0,0 +1,16 @@
+function makeHTML () {
+    console.log("Thank you for using Team Generator! Your HTML document is being written now...")
+
+    const templateArray = []
+    const initialHTML = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Profile</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <style></style>
+</head>`
+}
